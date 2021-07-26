09d6a07f0a8890e6a6fd5a656a34019909aea4a5
helei <2215694729@qq.com>
2021-07-26 17:44:51 +0800

    更新GUI绘图内容

--- gui4design/src/main/java/org/corejavavolume/gui/Draw2DShapes.java
+++ gui4design/src/main/java/org/corejavavolume/gui/Draw2DShapes.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
+import java.io.File;
 
 /**
  * 根据例子，画椭圆，举行，线段，园
@@ -22,14 +23,24 @@ public class Draw2DShapes {
 
 class DrawFrame extends JFrame {
     public DrawFrame(){
-        add(new DrawComponent());
+//        add(new DrawComponent());
+//        add(new TitlePanel());
+        add(new ImageComponent());
         pack();
     }
 }
 
 class DrawComponent extends JComponent {
-    private static final int DEFAULT_WIDTH = 400;
-    private static final int DEFAULT_HEIGHT = 400;
+    private final int DEFAULT_WIDTH;
+    private final int DEFAULT_HEIGHT;
+
+    public DrawComponent(){
+        setSize(600, 600);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        DEFAULT_WIDTH = dimension.width;
+        DEFAULT_HEIGHT = dimension.height;
+    }
 
     public void paintComponent(Graphics g){
         Graphics2D graphics2D = (Graphics2D) g;
@@ -58,14 +69,79 @@ class DrawComponent extends JComponent {
         //draw a circle with the same center
         double centerX = rect.getCenterX();
         double centerY = rect.getCenterY();
+        System.out.println(centerX+" ---- "+centerY);
         double radius = 150;
 
         Ellipse2D circle = new Ellipse2D.Double();
         circle.setFrameFromCenter(centerX, centerY, centerX+radius, centerY+radius);
         graphics2D.draw(circle);
 
+        //draw image
+
+        String dir = System.getProperty("user.dir") + "\\gui4design\\src\\main\\resources\\environment.jpg";
+        dir.replace("/", "\\");
+        System.out.println(dir);
+        File file = new File(dir);
+        if(file.exists()){
+            System.out.println("找到了图片了！");
+        } else {
+            System.out.println("未找到文件");
+        }
+        Image image = new ImageIcon(dir).getImage();
+        graphics2D.drawImage(image, 200, 600, null);
+
     }
 
+    public Dimension getPreferredSize(){
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+}
+
+class ImageComponent extends JComponent {
+    private static final int DEFAULT_WIDTH = 900;
+    private static final int DEFAULT_HEIGHT = 700;
+    private Image image;
+
+    public ImageComponent(){
+        String dir = System.getProperty("user.dir") + "\\gui4design\\src\\main\\resources\\icon.gif";
+        image = new ImageIcon(dir).getImage();
+    }
+
+    public void paintComponent(Graphics graphics){
+        if(image == null) return;
+        int imgWidth = image.getWidth(this);
+        int imgHeight = image.getHeight(this);
+
+        graphics.drawImage(image, 0, 0, null);
+
+        //上边是画一个图，下边循环是将图片铺满整个屏幕
+        for(int i=0; i * imgWidth <= getWidth(); i++){
+            for (int j=0; j* imgHeight <= getHeight(); j++){
+                if(i+j > 0){
+                    graphics.copyArea(0, 0, imgWidth, imgHeight, i * imgWidth, j * imgHeight);
+                    try {
+                        Thread.sleep(50 * 1L);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public Dimension getPreferredSize(){
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+}
+
+class TitlePanel extends JPanel{
+    private final int DEFAULT_WIDTH = 50;
+    private final int DEFAULT_HEIGHT = 200;
+    public void paintComponent(Graphics g){
+        setSize(200, 50);
+        String dir = System.getProperty("user.dir");
+        g.drawString(dir, 10, 20);
+    }
     public Dimension getPreferredSize(){
         return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
     }
