446e5058160084141d4563243ef930d9ec286ca1
NeverHeartache <2215694729@qq.com>
2021-08-23 23:23:22 +0800

    添加自定义桌面应用程序开发，初步设计书

---
+++ gui4design/src/main/java/org/application/combat/ApplicationBoot.java
@@ -0,0 +1,12 @@
+package org.application.combat;
+
+/**
+ * 项目实战启动类
+ * @author dell
+ * @version 1.0.1
+ */
+public class ApplicationBoot {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ gui4design/src/main/java/org/application/combat/layout/TopLayout.java
@@ -0,0 +1,9 @@
+package org.application.combat.layout;
+
+import javax.swing.*;
+
+public class TopLayout extends JFrame {
+    public TopLayout() {
+
+    }
+}
