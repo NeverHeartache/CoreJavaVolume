641e389ba6dbd3e715200cb5e12e2ee1f9b43a7a
helei <2215694729@qq.com>
2022-02-23 17:33:47 +0800

    添加线程局部变量；同步阻塞代码；

--- ch_114_concurrent/src/main/java/org/corejavavolume/concurrent/basic/BasicThread.java
+++ ch_114_concurrent/src/main/java/org/corejavavolume/concurrent/basic/BasicThread.java
@@ -1,8 +1,25 @@
 package org.corejavavolume.concurrent.basic;
 
-public class BasicThread implements Runnable{
-    @Override
-    public void run() {
-
+public class BasicThread {
+    public static void main(String[] args) {
+        Runnable r = () -> {
+            SyncronizedBlock sb = new SyncronizedBlock();
+            sb.blockTest();
+        };
+        for (int i=0; i< 1000; i++) {
+            Thread t = new Thread(r);
+            t.start();
+        }
+    }
+}
+class SyncronizedBlock {
+    private Object lock = new Object();
+    public void blockTest() {
+        String threadName = Thread.currentThread().getName();
+        System.out.println(threadName + "This is unsync code.(非同步代码)");
+        synchronized(lock) {
+            System.out.println(threadName + " : This is sync code.（同步代码）");
+        }
+        System.out.println(threadName + "This is unsync code.（非同步代码）");
     }
 }
