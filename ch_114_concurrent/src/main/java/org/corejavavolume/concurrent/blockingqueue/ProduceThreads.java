57998aa2b7591b3def8397908e9c67c9c21056a1
helei <2215694729@qq.com>
2022-02-25 17:33:21 +0800

    阻塞队列仍然没写出例子来

--- ch_114_concurrent/src/main/java/org/corejavavolume/concurrent/blockingqueue/ProduceThreads.java
+++ ch_114_concurrent/src/main/java/org/corejavavolume/concurrent/blockingqueue/ProduceThreads.java
@@ -6,13 +6,15 @@ import java.util.concurrent.LinkedBlockingQueue;
  * 生产者线程集
  */
 public class ProduceThreads {
-    private final int naccout = 100;
+    private final int I_ZH_ZYXMKXZFSQ = 100;
 
-    public String sendThreadToQueue(){
-        return null;
+    public String sendThreadToQueue(Thread thread){
+        return thread.getName();
     }
 
-    public static void main(String[] args) {
-
+    public void madeSomeThreads(){
+        Runnable r = () -> {
+            ProduceThreads  produceThreads = new ProduceThreads();
+        };
     }
 }
