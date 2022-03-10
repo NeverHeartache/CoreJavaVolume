d48efa05da7a52a771261e50c1f408247ea271a8
helei <2215694729@qq.com>
2022-03-10 15:34:55 +0800

    开始学习Fork-Join框架

--- ch_114_concurrent/src/main/java/org/corejavavolume/concurrent/forkjoin/ForkJoinFrame.java
+++ ch_114_concurrent/src/main/java/org/corejavavolume/concurrent/forkjoin/ForkJoinFrame.java
@@ -1,4 +1,20 @@
 package org.corejavavolume.concurrent.forkjoin;
 
+import java.util.concurrent.RecursiveTask;
+
 public class ForkJoinFrame {
+    public static void main(String[] args) {
+        final int SIZE = 10_000_000;
+        double[] numbers = new double[SIZE];
+        for (int i=0; i < SIZE; i++) numbers[i] = Math.random();
+//        Counter counter
+    }
+}
+class Counter extends RecursiveTask<Integer> {
+    public static final int THRESHOLD = 1000;
+
+    @Override
+    protected Integer compute() {
+        return null;
+    }
 }
